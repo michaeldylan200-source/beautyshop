4218090ed39f695450bfca0990c04286c35bc585
michaeldylan200-source <michaeldylan200@gmail.com>
2025-12-02 13:47:47 -0800

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,18 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDhkpoM-7_Ns_YTWkDSw-8xWKkAUw6N0PE",
+  authDomain: "adani-beauty-and-wellness.firebaseapp.com",
+  databaseURL: "https://adani-beauty-and-wellness-default-rtdb.firebaseio.com",
+  projectId: "adani-beauty-and-wellness",
+  storageBucket: "adani-beauty-and-wellness.firebasestorage.app",
+  messagingSenderId: "211419892764",
+  appId: "1:211419892764:web:b5648ce96e7856720163d9"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
